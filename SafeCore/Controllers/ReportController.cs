bba29c5010a7bf86a0224e0f0382ebee879e62b3
Doppelsito <maur.barriga@alumnos.duoc.cl>
2022-09-13 18:51:50 -0300

    Creacion ReportController y Creacion vista ReportClient

---
+++ SafeCore/Controllers/ReportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SafeCore.Controllers
+{
+    public class ReportController : Controller
+    {
+        public IActionResult ReportClient()
+        {
+            return View();
+        }
+    }
+}
