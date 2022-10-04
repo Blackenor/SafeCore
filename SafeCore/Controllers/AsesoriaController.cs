547c556c367ec94729f385e1b333dad438b53932
Doppelsito <maur.barriga@alumnos.duoc.cl>
2022-10-04 17:48:56 -0300

    Creacion de controladores y vistas de todo el menu

---
+++ SafeCore/Controllers/AsesoriaController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SafeCore.Controllers
+{
+    public class AsesoriaController : Controller
+    {
+        public IActionResult IngresarAsesoria()
+        {
+            return View();
+        }
+        public IActionResult AsesoriaEspecial()
+        {
+            return View();
+        }
+    }
+}
---
+++ SafeCore/Controllers/ChecklistController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SafeCore.Controllers
+{
+    public class ChecklistController : Controller
+    {
+        public IActionResult Crearcheck()
+        {
+            return View();
+        }
+        public IActionResult Responder()
+        {
+            return View();
+        }
+    }
+}
